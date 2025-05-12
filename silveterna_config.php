ba4fbc8b8e1b02de20279af8c019b8e3c2ee7186
Issam-BH <issambh44@gmail.com>
2025-05-12 16:52:19 +0200

    Modif lien calendrier, jeux et option

--- silveterna_config.php
+++ silveterna_config.php
@@ -6,7 +6,7 @@ $config = [
     'host' => 'localhost',
     'dbname' => 'silveterna',
     'user' => 'postgres',
-    'password' => '2606',
+    'password' => '1307',
 ];
 
 // Fonction pour établir la connexion à la base de données
